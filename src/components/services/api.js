97aa2373c90256e23689fc4eaa0c27ab12c7c0f4
Daniel Avila <danielsantiago1002@hotmail.com>
2021-07-04 22:51:41 -0500

    Update api.js

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const token ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzg5ZDkxNWM0MDcxNWU1YzZkOTcxNTQ1NDYxZTJiYiIsInN1YiI6IjYwZTBiMzBiMjU4ODIzMDA1ZDc5N2VkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FNRlAAOXPAIucTYplgDTonJn91pGp7IM7b1Jp54SP5M"
+const token =""
 
 export default axios.create({
     baseURL:"https://api.themoviedb.org/3/",
